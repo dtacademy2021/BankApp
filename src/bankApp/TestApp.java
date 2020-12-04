73a0d278e3ced168ecba954f90e0abdfc3e77dcd
Duotech Academy <duotechinstructor@gmail.com>
2020-12-03 20:31:00 -0500

    Java files

---
+++ src/bankApp/TestApp.java
@@ -0,0 +1,61 @@
+package bankApp;
+
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 
+ * This Runner class is used to obtain customer information from a text file 
+ * and create multiple bank accounts based on it and display information about them
+ * 
+ *
+ */
+
+public class TestApp {
+	
+	public static void main(String[] args) throws FileNotFoundException {
+		
+		//1. Create a List of Accounts and initialize it
+		//Ex:
+			List<Account> accounts = new ArrayList<>();
+		
+		
+		//2. Create a List of customers
+		//Read new account holder information from a file customers.txt by calling the utility method readFromAFile()
+		//and store it into a List of String[] 
+		
+		List<String[]> customers = Utility.readFromAFile("customers.txt");
+		
+		
+		//3. Add new accounts to List of Accounts		
+		//Loop through the List of customers and get the information for each new customer (name, ssn, accountType, initialBalance )
+		// and create a new Savings or Checking account based on the accountType and add that new Account to the List of Accounts
+		
+		
+		for (String[] customer : customers) {
+			System.out.println(customer[0]);
+		}
+		
+		
+		
+		
+		
+		
+		
+		//4. Loop through the List of Accounts and 
+		// call showInfo() method on each account to display info about each created account
+		
+		for (Account account : accounts) {
+			account.showInfo();
+		}
+		
+
+
+		
+
+
+		
+	}
+
+}
